6bcd085f6ae3b9ac2e02c3fe478700ad9be0d3a9
Feng-420 <76527150+Feng-420@users.noreply.github.com>
2020-12-24 16:03:21 +0800

    Update Student.java

--- src/com/yidu/bean/Student.java
+++ src/com/yidu/bean/Student.java
@@ -2,7 +2,6 @@ package com.yidu.bean;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("hello,git");
-        private Int age=19;
+        System.out.println("hellogit");
     }
 }
