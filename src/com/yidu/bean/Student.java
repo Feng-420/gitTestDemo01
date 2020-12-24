6f17cf4b45b1961017bdfdc22290b07acb0d10e4
Feng-420 <2819688753@qq.com>
2020-12-24 16:12:51 +0800

    我新增的老师类第二次

--- src/com/yidu/bean/Student.java
+++ src/com/yidu/bean/Student.java
@@ -3,5 +3,7 @@ package com.yidu.bean;
 public class Student {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("hello,git");
+
     }
 }
